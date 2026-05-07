196f688f3342fe5781e7ba02e17c6ceeffe5681d
RISHAV KISHORE <rishavkishore732@gmail.com>
2026-05-07 12:54:37 +0530

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool courseScheduletopo(int n, unordered_map<int, vector<int>>&adj, vector<int>&indegree){
+        queue<int> q;
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                count++;
+                q.push(i);
+            }
+        }
+
+        while(!q.empty()){
+            int m=q.front();
+            q.pop();
+
+            for(int &v:adj[m]){
+                indegree[v]--;
+                if(indegree[v]==0){
+                    q.push(v);
+                    count++;
+                }
+            }
+
+        }
+
+        if(count==n) return true;
+
+        return false;
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int, vector<int>>adj;
+        vector<int>indegree(numCourses, 0);
+
+        for(auto &vec:prerequisites){
+            int a=vec[0];
+            int b=vec[1];
+
+            adj[b].push_back(a);
+            indegree[a]++;
+        }
+
+        return courseScheduletopo(numCourses, adj, indegree);
+    }
+};
